6c5bd681c13116c99b2c4eefe1c9c4123d5b938a
dairon20jb <dairon.barbosa@javeriana.edu.co>
2020-11-30 17:29:09 -0500

    Edit
    Coment

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-
-
-import cv2
 import os
 from tkinter import *
 import cv2
 from PIL import Image, ImageTk
 import imutils
 import shutil
-window = Tk()
+window = Tk()  # Se crea la ventana de la interfaz
 window.title("Reconocimiento Facial")
-window.geometry('200x165')
+window.geometry('200x165') #Dimensiones iniciales de la ventana
 width, height = 200, 165
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(1) #Seleccionando la cámara que se usará para la implementación
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 lbl = Label(window, text="Digite cantidad de fotos:")
-# filename = PhotoImage(file = "C:\PRUEBA\circuit.png")
-# background_label = Label(window, image=filename)
-# background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-dataPath = 'C:\PRUEBA\Data'  # Cambia a la ruta donde hayas almacenado Data
+dataPath = 'C:\PRUEBA\Data'  # Cambia a la ruta donde quieras que se almacene Data
 imagePaths = os.listdir(dataPath)
 print('imagePaths=', imagePaths)
-
-
-face_recognizer = cv2.face.EigenFaceRecognizer_create()
+face_recognizer = cv2.face.EigenFaceRecognizer_create() #Método seleccionado
 # face_recognizer = cv2.face.FisherFaceRecognizer_create()
 # face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 # Leyendo el modelo
@@ -32,14 +23,15 @@ face_recognizer.read('modeloEigenFace.xml')
 # face_recognizer.read('modeloFisherFace.xml')
 # face_recognizer.read('modeloLBPHFace.xml')
 #cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-
-# cap = cv2.VideoCapture('kk.mp4')
+# cap = cv2.VideoCapture('prueba.mp4') #En caso de realizar pruebas con videos
+# Se hace uso de haarcascade para encontrar los rostros en la imagen
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 def show_frame():
+    #Redimensión de la ventana una vez se abre la Cámara
        window.geometry('325x400')
        width, height = 325,400
        lbl4 = Label(window, text="Digite cantidad de fotos:")
-       lbl.grid(column=0, row=0)
+       lbl.grid(column=0, row=0)  #Ubicación de los botones y texto de la interfaz
        btn.grid(column=0, row=4)
        btn1.grid(column=0, row=8)
        btn3.grid(column=0, row=12)
@@ -47,34 +39,30 @@ def show_frame():
        txt.grid(column=0, row=20)
        txt1.grid(column=0, row=28)
        lbl2.grid(column=0, row=24)
-
        ret, frame = cap.read()
        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
        auxFrame = gray.copy()
-       faces = faceClassif.detectMultiScale(gray, 1.15, 5)
+       faces = faceClassif.detectMultiScale(gray, 1.15, 5) #Sensibilidad del detecto de rostros
        for (x, y, w, h) in faces:
               rostro = auxFrame[y:y + h, x:x + w]
               rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
               result = face_recognizer.predict(rostro)
               cv2.putText(frame, '{}'.format(result), (x, y - 5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
+              # Condiciones para encontrar un rostro conocido
               if result[1] < 5000:
                      cv2.putText(frame, '{}'.format(imagePaths[result[0]]), (x, y - 25), 2, 1.1,(0, 255, 0), 1, cv2.LINE_AA)
                      cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
               else:
                      cv2.putText(frame, 'Desconocido', (x, y - 20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
                      cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
+        # Abrir la camara en la interfaz
        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
        img = Image.fromarray(cv2image)
        imgtk = ImageTk.PhotoImage(image=img)
        lbl.imgtk = imgtk
        lbl.configure(image=imgtk)
-       k = cv2.waitKey(1)
        lbl.after(10, show_frame)
-
-       #cap.release()
-       #cv2.destroyAllWindows()
-def clicked():
+def fotos():
        personName = txt1.get()
        personPath = dataPath + '/' + personName
        if not os.path.exists(personPath):
@@ -85,27 +73,25 @@ def clicked():
        faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
        count = 0
        while True:
-
            ret, frame = cap.read()
            if ret == False: break
            frame = imutils.resize(frame, width=640)
            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
            auxFrame = frame.copy()
-           faces = faceClassif.detectMultiScale(gray, 1.15 , 5)
+           faces = faceClassif.detectMultiScale(gray, 1.15 , 5) #Sensibilidad del detector de rostros
+           # Recorta el rostro de cada frame y lo almacena
            for (x, y, w, h) in faces:
-               cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+               cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) #Dibujar rectangulo alrededor del rostro
                rostro = auxFrame[y:y + h, x:x + w]
                rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
                cv2.imwrite(personPath + '/rotro_{}.jpg'.format(count), rostro)
                count = count + 1
            cv2.imshow('frame', frame)
-           k = cv2.waitKey(1)
+           k = cv2.waitKey(1)   #Se repite hasta que se capturen la cantidad de fotos deseadas por el usuario
            if k == 27 or count >= (int(txt.get())):
                break
        cap.release()
        cv2.destroyAllWindows()
-
-
 def entrenar():
     peopleList = os.listdir(dataPath)
     print('Lista de personas: ', peopleList)
@@ -135,21 +121,18 @@ def entrenar():
     print('Número de etiquetas 8: ',np.count_nonzero(np.array(labels)==4))
     print('Número de etiquetas 9: ',np.count_nonzero(np.array(labels)==4))
     print('Número de etiquetas 10: ',np.count_nonzero(np.array(labels)==4))
-
     face_recognizer = cv2.face.EigenFaceRecognizer_create()
     # Entrenando el reconocedor de rostros
-    print("Entrenando...")
+    print("Entrenando...")  #Se entrena el método y una vez entrenado se almacena
     face_recognizer.train(facesData, np.array(labels))
     face_recognizer.write('modeloEigenFace.xml')
     #face_recognizer.write('modeloFisherFace.xml')
     #face_recognizer.write('modeloLBPHFace.xml')
     print("Modelo almacenado...")
-
-
 txt = Entry(window,width=10)
 txt1 = Entry(window,width=10)
 btn = Button(window, text="Iniciar reconocimiento", command=show_frame)
-btn1 = Button(window, text="Tomar fotos para entrenar método", command=clicked)
+btn1 = Button(window, text="Tomar fotos para entrenar método", command=fotos)
 btn3 = Button(window, text="Entrenar método", command=entrenar)
 lbl2 = Label(window, text="Nombre:")
 btn.grid(column=0, row=0)
